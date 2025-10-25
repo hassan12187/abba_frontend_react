27bb94989526a666f99f1c3fa97d0e5c3276c216
muhammad hassan <hassanmuhammad12187@gmail.com>
2025-10-26 02:32:03 +0500

    complaints changes

--- src/components/hooks/useCustomQuery.jsx
+++ src/components/hooks/useCustomQuery.jsx
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
 import { GetService } from "../../Services/Services"
 
-const useCustomQuery=(route,token,queryKey)=>{
+const useCustomQuery=(route,token,...key)=>{
     return useQuery({
-        queryKey:[queryKey],
+        queryKey:[key],
         queryFn:async()=>await GetService(route,token),
         staleTime: 1000 * 60 * 60,
         cacheTime: 1000 * 60 * 60,
--- src/pages/Complaints/Complaints.jsx
+++ src/pages/Complaints/Complaints.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "../../components/Layout/Pagination";
 import useCustomQuery from "../../components/hooks/useCustomQuery";
 import { useCustom } from "../../Store/Store";
+import FilterSection from "../../components/reusable/FilterSection";
+import InputField from "../../components/reusable/InputField";
+import SelectField from "../../components/reusable/SelectField";
+import { useDebounce } from "../../components/hooks/useDebounce";
 
 const Complaints=()=>{
     const {token}=useCustom();
+    const [instantVal,setInstantVal]=useState({
+      room_no:"",
+      category:"",
+      status:""
+    });
+    const [filterVal,setFilterVal]=useState({ 
+       room_no:"",
+      category:"",
+      status:""});
       const [currentPage, setCurrentPage] = useState(1);
-      const {data}=useCustomQuery(`/api/admin/complaint`,token,'complaints');
+      const {data}=useCustomQuery(`/api/admin/complaint?room_no=${filterVal.room_no}&category=${filterVal.category}&status=${filterVal.status}`,token,filterVal.room_no,filterVal.category,filterVal.status);
+      const updateFilter=useDebounce((name,value)=>{
+        setFilterVal((prev)=>{
+          return {...prev,[name]:value};
+        })
+      },500);
+      const handleFilterChange=(e)=>{
+        const {name,value}=e.target;
+        setInstantVal((prev)=>{
+          return {...prev,[name]:value};
+        });
+        updateFilter(name,value);
+      };
       console.log(data);
     return <div className="expenses-page">
            <div className="page-header">
@@ -16,6 +41,23 @@ const Complaints=()=>{
         </h2>
         <p>Track and manage all hostel complaints</p>
       </div>
+      <FilterSection heading={"Filter Complaints"}>
+        <InputField type={"text"} name={"room_no"} value={instantVal.room_no} onChange={handleFilterChange} id={"room_no"} label={"Room Number"} placeholder={"Enter Room Number"}  />
+        <SelectField name={"category"} value={instantVal.category} onChange={handleFilterChange} id={"category"} label={"Complain Type"}>
+          <option >Select Any Category</option>
+          <option value={'Electrical'}>Electrical</option>
+          <option value={'Plumbing'}>Plumbing</option>
+          <option value={'Cleaning'}>Cleaning</option>
+          <option value={'Furniture'}>Furniture</option>
+          <option value={'Other'}>Other</option>
+        </SelectField>
+        <SelectField name={"status"} id={"status"} value={instantVal.status} onChange={handleFilterChange} label={"Complain Status"}>
+          <option >Select Any Status</option>
+          <option value={'Pending'}>Pending</option>
+          <option value={'Resolved'}>Resolved</option>
+          <option value={'In Progress'}>In Progress</option>
+        </SelectField>
+      </FilterSection>
             <div className="expenses-table-section">
         <div className="section-card">
           <div className="card-header-enhanced">
@@ -37,7 +79,6 @@ const Complaints=()=>{
                     <th>title</th>
                     <th>category</th>
                     <th className="amount-column">status</th>
-                    <th className="date-column">Assigned To</th>
                     <th className="actions-column">Actions</th>
                   </tr>
                 </thead>
